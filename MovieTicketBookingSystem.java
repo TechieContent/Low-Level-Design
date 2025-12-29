c15a38530416fd1fdf0d41442d6bf438d6c39618
TechieContent <janujahn1607@gmail.com>
2025-12-29 13:05:40 +0530

    Update MovieTicketBookingSystem.java

--- MovieTicketBookingSystem.java
+++ MovieTicketBookingSystem.java
@@ -1,3 +1,11 @@
+
+/*
+ * For detailed explanations, system design breakdown, and the UML diagram,
+ * check out my medium article at: https://medium.com/@techiecontent/day-8-design-a-movie-ticket-booking-system-java-code-b1d2ee202e64
+ *
+/
+
+
 import java.util.*;
 import java.time.LocalDateTime;
 
